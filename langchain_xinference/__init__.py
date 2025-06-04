cef78320f5afa739b237a3b48bde37f8082e8b53
bash000000 <m2588953@outlook.com>
2025-06-04 23:03:47 +0800

    Update reranks and embeddings

--- langchain_xinference/__init__.py
+++ langchain_xinference/__init__.py
@@ -2,7 +2,8 @@ from importlib import metadata
 
 from langchain_xinference.chat_models import ChatXinference
 from langchain_xinference.llms import Xinference
-
+from langchain_xinference.embeddings import XinferenceEmbeddings
+from langchain_xinference.reranks import XinferenceRerank
 
 try:
     __version__ = metadata.version(__package__)
@@ -14,5 +15,7 @@ del metadata  # optional, avoids polluting the results of dir(__package__)
 __all__ = [
     "ChatXinference",
     "Xinference",
+    "XinferenceEmbeddings",
+    "XinferenceRerank",
     "__version__",
 ]
